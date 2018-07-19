3ec2720065d97a17bdf9a4c8b1b8aa199fdb95d4
randilfernando <randil.fernando@outlook.com>
2018-07-20 01:35:45 +0530

    Added Member class (use with msstack servers)

--- src/main/java/com/grydtech/msstack/core/serviceregistry/Member.java
+++ src/main/java/com/grydtech/msstack/core/serviceregistry/Member.java
@@ -4,13 +4,27 @@ import org.springframework.cloud.zookeeper.discovery.ZookeeperInstance;
 
 import java.util.Map;
 
+@SuppressWarnings("all")
 public class Member extends ZookeeperInstance {
+
+    private String host;
+    private Integer port;
 
     private Member() {
         super(null, null, null);
     }
 
-    public Member(String id, String name, Map<String, String> metadata) {
+    public Member(String id, String name, String host, Integer port, Map<String, String> metadata) {
         super(id, name, metadata);
+        this.host = host;
+        this.port = port;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public Integer getPort() {
+        return port;
     }
 }
